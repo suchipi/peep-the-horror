6ca532d01243a2a87a281db9a34020f8eff29b1a
Lily Scott <git@suchipi.tv>
2022-01-02 10:23:35 -0700

    add focus grabber TODO

--- src/useFocusGrabber.tsx
+++ src/useFocusGrabber.tsx
@@ -7,6 +7,7 @@ export default function useFocusGrabber() {
         document.activeElement &&
         document.activeElement.tagName === "IFRAME"
       ) {
+        // TODO: this absolutely sucks if you're using tab to navigate through the document
         console.log("Refocusing away from iframe...");
         document.body.focus();
       }
